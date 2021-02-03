f8655789eecfa67d002a9c754a148b05a404a311
andreaskuuskaru <Andreas.Kuuskaru@tptlive.ee>
2021-02-03 15:22:06 +0200

    Tunnitöö

---
+++ tund1/kolmnurk.c
@@ -0,0 +1,25 @@
+//
+// Created by andreas on 03.02.21.
+/*
+ *  Ülesanne 1
+    Kirjuta programm, mis sisestab täisnurkse kolmnurga kaatetite pikkused ning väljastab selle kolmnurga pindala
+    ja hüpotenuusi pikkuse. Kas kolmnurga pindala tuleb alati õige, kui kaatetid on määratud täisarvulistena? Näiteks
+    kui mõlemad kaatetid on paaritud arvud? Miks? Mida teha?
+ */
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("test");
+
+    int kaatet1, kaatet2;
+    float hypotenuus, pindala;
+    printf("Sisesta kaatetid ");
+    scanf("%d %d", &kaatet1, &kaatet2);
+    printf()
+
+}
+
+
+
---
+++ tund1/ruutvorrand.c
@@ -0,0 +1,20 @@
+//
+// Created by andreas on 03.02.21.
+//
+
+/*
+ * Ülesanne 2
+    Kirjuta programm, mis sisestab ruutvõrrandi
+    ax2 + bx + c = 0
+    kordajad ja leiab selle reaalarvulised lahendid (kui need on olemas) või väljastab korrektse teate
+    (kui võrrandil reaalarvulised lahendid puuduvad).
+    Muide, ruutvõrrandi saab ka nö võrrandi kujul (ax2 + bx + c = 0) sisestada ja sellest formaadi abil kordajad
+    lasta välja noppida. Proovi!
+ */
+
+#include <stdio.h>
+
+int main() {
+
+}
+
